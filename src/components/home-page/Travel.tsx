fde1ca1e9930213e7aa734331c835961e3f15d9d
Martins Bruvelis <martins.bruvelis@kaust.edu.sa>
2025-10-22 17:37:50 +0300

    Add Travel section.

---
+++ src/components/home-page/Travel.tsx
@@ -0,0 +1,42 @@
+import ArrowIcon from '../../assets/icons/arrow.svg?raw';
+import type { ClientConfig } from '@thebcms/client';
+import { BCMSImage } from '@thebcms/components-react';
+import type {
+    PropMediaDataParsed,
+    PropRichTextDataParsed,
+} from '@thebcms/types';
+import ContentManager from '../ContentManager';
+
+interface Props {
+    title: string;
+    description: PropRichTextDataParsed;
+    bcmsConfig: ClientConfig;
+}
+
+const HomeTravel: React.FC<Props> = ({
+    title,
+    description,
+    bcmsConfig,
+}) => {
+    return (
+        <section className="pt-8 pb-16 lg:pt-[120px] lg:pb-[120px]">
+            <div className="container">
+                <div className="flex items-center justify-between mb-4 lg:mb-[72px]">
+                    <h2 id="travel" className="scroll-mt-24 md:scroll-mt-32 leading-none tracking-[-0.05em] font-semibold lg:text-[104px] lg:leading-none">
+                        <h2>{title}</h2>
+                    </h2>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: ArrowIcon }}
+                        className="w-4 h-4 lg:w-[100px] lg:h-[104px]"
+                    />
+                </div>
+                <ContentManager
+                    items={description.nodes}
+                    className="text-sm leading-[1.4] tracking-[-0.8px] text-appGray-500 font-medium mb-6 lg:text-[26px] lg:leading-[1.4] lg:mb-[112px]"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default HomeTravel;
