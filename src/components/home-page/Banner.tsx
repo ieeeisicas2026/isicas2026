a85eb06ef773a3d23a7b131285fb349d92464ac6
Martins Bruvelis <martins.bruvelis@kaust.edu.sa>
2026-03-05 12:04:49 +0300

    Add floating banner.

---
+++ src/components/home-page/Banner.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+import type { PropRichTextDataParsed } from '@thebcms/types';
+import type { ClientConfig } from '@thebcms/client';
+import ContentManager from '../ContentManager';
+
+interface Prop {
+    description: PropRichTextDataParsed;
+    bcmsConfig: ClientConfig;
+}
+
+const HomeBanner: React.FC<Prop> = ({
+    description,
+    bcmsConfig,
+}) => {
+    if (!description) {
+    return (none);
+    }
+
+    return ( 
+        <section id="marketing-banner" className="fixed z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 lg:p-8 -translate-x-1/2 bg-white bg-opacity-95 border rounded-lg shadow-xs lg:max-w-7xl left-1/2 top-20 md:top-28 bg-yellow-50 dark:bg-gray-800 ">
+            <div class="flex flex-col items-start md:items-center md:flex-row">
+                <ContentManager
+                    items={description.nodes}
+                    className="text-sm text-yellow-800 dark:text-yellow-300 leading-[1.4] tracking-[-0.41px] max-w-[823px] mx-auto lg:text-2xl lg:leading-none"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default HomeBanner;
