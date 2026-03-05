68dab26914176a36468769c0a9fd0b0560e313c9
Martins Bruvelis <martins.bruvelis@kaust.edu.sa>
2026-03-05 12:08:38 +0300

    Fix class => className.

--- src/components/home-page/Banner.tsx
+++ src/components/home-page/Banner.tsx
@@ -19,7 +19,7 @@ const HomeBanner: React.FC<Prop> = ({
 
     return ( 
         <section id="marketing-banner" className="fixed z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 lg:p-8 -translate-x-1/2 bg-white bg-opacity-95 border rounded-lg shadow-xs lg:max-w-7xl left-1/2 top-20 md:top-28 bg-yellow-50 dark:bg-gray-800 ">
-            <div class="flex flex-col items-start md:items-center md:flex-row">
+            <div className="flex flex-col items-start md:items-center md:flex-row">
                 <ContentManager
                     items={description.nodes}
                     className="text-sm text-yellow-800 dark:text-yellow-300 leading-[1.4] tracking-[-0.41px] max-w-[823px] mx-auto lg:text-2xl lg:leading-none"
